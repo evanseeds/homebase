cb228d952991f78bdf27b48d873218bd7c2daa1a
Evan Seeds <evanseeds@gmail.com>
2017-10-02 16:33:58 -0400

    Implement basic (VERY BASIC) unit test

---
+++ webapp/unit-tests.ts
@@ -0,0 +1,6 @@
+// Can't use ES5 features because we'll be running this with PhantomJS
+describe('First unit test', function() {
+	it('should run as expected', function() {
+		expect(true).toBeTruthy;
+	});
+});
